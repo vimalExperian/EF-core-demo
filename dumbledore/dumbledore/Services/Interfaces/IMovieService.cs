13cc8932e4b4477ca3e8dc111c96668dbeb3dd97
SubikshaRamkumar <subikshakr11.12.2003@gmail.com>
2025-02-28 12:38:59 +0530

    Corrected IMovieService

--- dumbledore/dumbledore/Services/Interfaces/IMovieService.cs
+++ dumbledore/dumbledore/Services/Interfaces/IMovieService.cs
@@ -4,7 +4,7 @@ namespace dumbledore.Services.Interfaces
 {
     public interface IMovieService
     {
-        public CreateMovieResponse AddMovie(CreateMovierRequest createMovierRequest);
+        public void AddMovie(CreateMovierRequest createMovierRequest);
 
         public CreateMovierRequest FetchMovie(int MovieID);
     }
